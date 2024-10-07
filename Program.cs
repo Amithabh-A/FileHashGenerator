36ff9d5d44909450c8c56d8d85ea8d2f0c1a1e91
Amithabh-A <amithabh2002@gmail.com>
2024-10-07 17:22:55 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    if (args.Length != 1)
+    {
+      Console.WriteLine("Usage: dotnet run <directory_path>");
+      return;
+    }
+
+    string directoryPath = args[0];
+
+    if (!Directory.Exists(directoryPath))
+    {
+      Console.WriteLine($"Directory does not exist: {directoryPath}");
+      return;
+    }
+
+    foreach (var filePath in Directory.GetFiles(directoryPath))
+    {
+      string hash = GetFileHash(filePath);
+      Console.WriteLine($"{Path.GetFileName(filePath)}: {hash}");
+    }
+  }
+
+  static string GetFileHash(string filePath)
+  {
+    using (var sha256 = SHA256.Create())
+    using (var stream = File.OpenRead(filePath))
+    {
+      byte[] hashBytes = sha256.ComputeHash(stream);
+      return BitConverter.ToString(hashBytes).Replace("-", "").ToLowerInvariant();
+    }
+  }
+}
+
